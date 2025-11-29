c680675275bca67e018056dac74df96c6043188b
rissiakou <rachel.issiakou@ensae.fr>
2025-11-29 14:46:08 +0000

    Je ne veux pas tout perdre

--- Mise en place des stats Desc.R
+++ Mise en place des stats Desc.R
@@ -1,12 +1,39 @@
 #Ne pas oublier de retélécharger le données à chaque fois 
 install.packages("dyplr")
+install.packages("stringr")
 install.packages("haven")
+install.packages("ISOweek")
+install.packages("forcats")
+library(forcats)
+library(ISOweek)
 library(haven)
 library(dplyr)
+library(stringr)
+
+
+# Mise en place des données ####
 
 data<- read_dta("eu-lfs 2023 sample.dta")
 
 data_modifiable <- data
+data_modifiable <- data_modifiable %>%
+  select(-refweek) 
+
+## On renomme le contenu des variabes ####
+
+data_modifiable <- data_modifiable %>%
+  mutate(
+    # Colonnes character
+    across(where(is.character),
+           ~ .x %>% na_if("Not available") %>% as_factor()),
+    
+    # Colonnes factor
+    across(where(is.factor),
+           ~ fct_na_value_to_level(.x, "Not available") %>% 
+             fct_recode(!!!setNames(rep(NA, 1), "Not available")))
+  )    
+
+View(data_modifiable)
 
 #Chaque colonne a un label qui explique le nom ESS de la colonne : je prends cette description comme nom de colonne
 # Extraire les labels
@@ -18,4 +45,6 @@ labels[sapply(labels, is.null) | labels == ""] <- names(data_modifiable)[sapply(
 # Appliquer les nouveaux noms
 names(data_modifiable) <- labels
 
-View(data_modifiable)
+name_vec <- names(data_modifiable)
+names(data_modifiable) <- make.unique(name_vec, sep = ".")
+
